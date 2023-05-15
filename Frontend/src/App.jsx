7972bece08dd5b3cda6d6f4407a42e6a3880b948
unfirthman <unfirthman@gmail.com>
2023-05-15 19:36:00 -0400

    great push, everyone

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,51 @@
 //App.jsx
 //import Calendar from './Components/Calendar/Calendar';
 //import Table from './Components/Table/Table';
-import './App.css'
-import ChsPourHouse from './Components/venueReact/chsPourHouse/chsPourHouse';
-import MusicFarm from './Components/venueReact/musicFarm/musicFarm';
-import MusicHall from './Components/venueReact/musicHall/musicHall';
-import RoyalAmerican from './Components/venueReact/royalAmerican/royalAmerican';
-import Theater99 from './Components/venueReact/theater99/theater99';
-import WindJammer from './Components/venueReact/windJammer/windJammer';
-
+import "./App.css";
+import ChsPourHouse from "./Components/venueReact/chsPourHouse/chsPourHouse";
+import MusicFarm from "./Components/venueReact/musicFarm/musicFarm";
+import MusicHall from "./Components/venueReact/musicHall/musicHall";
+import RoyalAmerican from "./Components/venueReact/royalAmerican/royalAmerican";
+import Theater99 from "./Components/venueReact/theater99/theater99";
+import WindJammer from "./Components/venueReact/windJammer/windJammer";
 
 const App = () => {
-
-  return (
-            <body>
-                <header>
-                    <h1>
-                        Welcome to CHS Source!
-                    </h1>
-                    <h2>Your place for independent venue schedules</h2>
-                </header>
-                <main>
-                    <div className='chs-pour-house'>
-                        <ChsPourHouse />
-                    </div>
-                    <div className='music-farm'>
-                        <MusicFarm />
-                    </div>
-                    <div className='music-hall'>
+    return (
+        <body>
+            <header>
+                <h1>Welcome to CHS Source!</h1>
+                <h2>Your place for independent venue schedules</h2>
+                <p>
+                    If you would like your venue added to this list, please contact the webmaster.
+                </p>
+            </header>
+            <main>
+                <div className="chs-pour-house">
+                    <ChsPourHouse />
+                </div>
+                <div className="music-farm">
+                    <MusicFarm />
+                </div>
+                <div className="music-hall">
                     <MusicHall />
-                    </div>
-                    <div className='royal-american'>
+                </div>
+                <div className="royal-american">
                     <RoyalAmerican />
-                    </div>
-                    <div className='theater-99'>
+                </div>
+                <div className="theater-99">
                     <Theater99 />
-                    </div>
-                    <div className='windjammer'>
+                </div>
+                <div className="windjammer">
                     <WindJammer />
-                    </div>
-                </main>
+                </div>
+            </main>
             <footer>
-                <a href='https://unfirthman.github.io/new-hanf-react-portfolio/'>Designed by michael hanf.</a>
+                <a href="https://unfirthman.github.io/new-hanf-react-portfolio/">
+                    Designed by michael hanf.
+                </a>
             </footer>
-            </body>
-  );
+        </body>
+    );
 };
 
 export default App;
